9dfa4d16a44a0040eee4106f7c96e70b5093b1a8
Yujin <jini1514@soongsil.ac.kr>
2023-05-23 21:32:43 +0900

    add: Practice2B-Retrofit으로 카카오 책 검색 API 다루기 (Retrofit, Moshi, OkHttp, Kapt)

---
+++ book-search-app/app/src/main/java/com/example/booksearchapp/ui/view/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.booksearchapp.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.booksearchapp.R
+import com.example.booksearchapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpBottomNavigationView()
+        if(savedInstanceState == null){// 앱이 처음 실행되었을때를 판정
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+        }
+    }
+
+    fun setUpBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SearchFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, FavoriteFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_setting -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
